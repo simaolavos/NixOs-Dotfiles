3237d737a0d940ec59ddfa9e650b7e0e7d58f8fe
Simao Lavos <simao.lavos@rnl.tecnico.ulisboa.pt>
2026-02-10 13:11:12 +0000

    fixed ez errors

--- flake.nix
+++ flake.nix
@@ -1,32 +1,29 @@
-
 {
 
-	description = "My Nix configurations";
+  description = "My Nix configurations";
 
-	inputs = {
-		nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
 
-		agenix = {
-			url = "github:ryantm/agenix";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-	};
+  };
 
-	outputs = { self, nixpkgs, ...}@inputs: {
-		let 
-			baseModules = [
-				inputs.agenix.nixosModules.default
-				inputs.home-manager.nixosModules.home-manager
-			];
-		in {
-			nixosConfigurations = {
-					rnl-simaolavos = nixpkgs.lib.nixosSystem {
-						system = "x86_64-linux";
-						specialArgs = { inherit inputs; };
-						modules = baseModules ++ [./hosts/rnl-simaolavos.nix];
-					};
-			};
-		};
-	};
-}
+    outputs = { self, nixpkgs, ...}@inputs:
+    let 
+      baseModules = [
+        inputs.agenix.nixosModules.default
+      ];
+    in {
+      nixosConfigurations = {
+        rnl-simaolavos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = baseModules ++ [./hosts/rnl-simaolavos.nix];
+        };
+      };
+    };
+  }
